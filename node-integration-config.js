69566b039b9d100debccfc4eea5f98e4726ae8f0
Kandarpa Galas <kandarpa.galas@gmail.com>
2023-01-16 14:47:08 -0300

    chore: add jest

---
+++ node-integration-config.js
@@ -0,0 +1,4 @@
+const config = require('./jest.config.mjs')
+config.testMatch = ['**/*.test.ts']
+
+module.exports = config
---
+++ node-unit-config.js
@@ -0,0 +1,4 @@
+const config = require('./jest.config.mjs')
+config.testMatch = ['**/*.spec.ts']
+
+module.exports = config
