f4b6e316c48acab12144c29002977009f13e2ede
kandarpagalas <kandarpa.galas@gmail.com>
2023-01-17 17:31:38 -0300

    chore: add jest configs for mongodb

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,6 +3,7 @@ export default {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
